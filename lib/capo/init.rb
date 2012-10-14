a643121d8af361bb4058aa610b039b6fdabb723e
Jeroen Jacobs <git@jeroenj.be>
2012-10-14 15:36:01 +0200

    Fixes typo in init messages

--- lib/capo/init.rb
+++ lib/capo/init.rb
@@ -12,7 +12,7 @@ module Capo
           repo = Git.open repository_path
           puts repo.lib.send :command, 'pull' # repo.pull is broken in git-1.2.5, see: https://github.com/schacon/ruby-git/issues/32
         rescue
-          puts "Reposito doesn't seem to exist yet..."
+          puts "Repository doesn't seem to exist yet..."
           puts "Cloning repository from #{repository_url} to #{repository_path}"
           Git.clone repository_url, repository_path
           retry
