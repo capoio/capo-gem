6641b7813f1201ec6a1a7faa1c3860ebf30bc6f3
Jeroen Jacobs <git@jeroenj.be>
2012-10-14 21:09:14 +0200

    Don't take longer than two seconds for git related tasks

--- lib/capo/init.rb
+++ lib/capo/init.rb
@@ -1,4 +1,5 @@
 require 'git'
+require 'timeout'
 
 module Capo
   module Init
@@ -8,13 +9,24 @@ module Capo
 
         puts "Pulling changes from remote repository"
         begin
-          repo = Git.open Capo.repository_path
-          puts repo.lib.send :command, 'pull' # repo.pull is broken in git-1.2.5, see: https://github.com/schacon/ruby-git/issues/32
+          repo = with_timeout{Git.open Capo.repository_path}
+          puts with_timeout{repo.lib.send :command, 'pull'} # repo.pull is broken in git-1.2.5, see: https://github.com/schacon/ruby-git/issues/32
         rescue
           puts "Repository doesn't seem to exist yet..."
           puts "Cloning repository from #{repository_url} to #{Capo.repository_path}"
-          Git.clone repository_url, Capo.repository_path
+          with_timeout{Git.clone repository_url, Capo.repository_path}
           retry
+        end
+      end
+
+      private
+      def with_timeout
+        begin
+          Timeout::timeout 2 do
+            yield
+          end
+        rescue Timeout::Error
+          puts 'Git timeout. Skipping repository update.'
         end
       end
     end
